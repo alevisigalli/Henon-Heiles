b3e275fccd5544f199096776177fbeba49154a97
Alessia Visigalli <alessia.visigalli@iit.it>
2023-10-16 17:54:58 +0200

    add figure with all the solutions

--- henon_heiles.py
+++ henon_heiles.py
@@ -27,6 +27,7 @@ import numpy as np
 from integration_methods import runge_kutta2, runge_kutta4, leap_frog, euler
 from var import equations_motion as var
 import poincare_maps as pm
+import matplotlib.pyplot as plt
 
 def henon_heiles(integration_method,f, y0, t_values, dt):
     num_steps = len(t_values)
@@ -37,7 +38,6 @@ def henon_heiles(integration_method,f, y0, t_values, dt):
         y_values[1] = y_values[0] + dt*np.array(f(t_values[0], y_values[0]))
         for i in range(2, num_steps):
             y_values[i] = leap_frog(f, t_values[i-1], y_values[i-1], y_values[i-2], dt)
-
     else:
         for i in range(1, num_steps):
             y_values[i] = integration_method(f, t_values[i-1], y_values[i-1], dt)
@@ -49,6 +49,7 @@ t_values = np.linspace(0, 200, 200001)
 
 # Choose a suitable step size
 dt = t_values[1] - t_values[0]
+print(dt)
 
 def main():
     parser = argparse.ArgumentParser(description="Henon-Heiles System Integration")
@@ -66,10 +67,11 @@ def main():
     traj_group.add_argument("--outer", action="store_true", help="Simulation of trajectory: outside separatrix")
     traj_group.add_argument("--torus", action="store_true", help="Simulation of trajectory: distorted torus")
     traj_group.add_argument("--hyperbolic", action="store_true", help="Simulation of trajectory: hyperbolic points (separatrices)")
+    traj_group.add_argument("--all", action="store_true", help="Simulation of trajectory: all the initial conditions")
 
     args = parser.parse_args()
 
-    selected_traj_options = [args.outer, args.torus, args.hyperbolic]
+    selected_traj_options = [args.outer, args.torus, args.hyperbolic, args.all]
     if sum(selected_traj_options) != 1:
         print("Error: You need to use exactly one of these three arguments: --outer, --torus, or --hyperbolic.")
         sys.exit(1)  # Exit with an error code
@@ -81,43 +83,61 @@ def main():
 
     # Define initial conditions
     if args.outer:
-        initial_conditions = [0, 0, -0.0428, -0.3438]
-        color = 'tab:blue'
-        title='Outside separatrix'
+        initial_conditions =  np.array([[0, 0, -0.0428, -0.3438]])
+        color = ['tab:blue']
+        title='Outside separatrix (E = 0.06)'
         print("Simulation of trajectory: outside separatrix")
         
     if args.torus:
-        initial_conditions = [0, -0.1475, 0.3101, 0]
-        color = 'tab:orange'
-        title = 'Distorted torus'
+        initial_conditions =  np.array([[0, -0.1475, 0.3101, 0]])
+        color = ['tab:orange']
+        title = 'Distorted torus (E = 0.06)'
         print("Simulation of trajectory: distorted torus")
         
     if args.hyperbolic:
-        initial_conditions = [0, 0.1563, 0.18876, -0.25]
-        color = 'tab:green'
-        title = 'Hyperbolic points: separatrices'
+        initial_conditions =  np.array([[0, 0.1563, 0.18876, -0.25]])
+        color = ['tab:green']
+        title = 'Hyperbolic points (E = 0.06): separatrices'
         print("Simulation of trajectory: separatrices")
+    
+    if args.all:
+        initial_conditions = np.array([[0, 0, -0.0428, -0.3438],[0, -0.1475, 0.3101, 0],[0, 0.1563, 0.18876, -0.25]])
+        color = ['tab:blue','tab:orange','tab:green']
+        title = 'All the 3 main initial conditions for E = 0.06'
+        print('All the 3 main initial conditions for E = 0.06')
     
     # Choose the integration algorithm
     if args.rk2:
         print("Integrate equations of motion using Runge-Kutta 2")
-        result = henon_heiles(runge_kutta2,var,initial_conditions, t_values, dt)
+        fig, axs = plt.subplots(1, 3, figsize=(15, 6), sharex=False, sharey=False)
+        for i in range(0,int(len(initial_conditions)),1):
+            result = henon_heiles(runge_kutta2,var,initial_conditions[i], t_values, dt)
+            pm.plot_henon_heiles(t_values,x=result[:,0], y=result[:,1], px=result[:,2], py=result[:,3],color=color[i],title=title,axs=axs)
+        plt.show()
 
     if args.rk4:
         print("Integrate equations of motion using Runge-Kutta 4")
-        result = henon_heiles(runge_kutta4,var,initial_conditions, t_values, dt)
+        fig, axs = plt.subplots(1, 3, figsize=(15, 6), sharex=False, sharey=False)
+        for i in range(0,int(len(initial_conditions)),1):
+            result = henon_heiles(runge_kutta4,var,initial_conditions[i], t_values, dt)
+            pm.plot_henon_heiles(t_values,x=result[:,0], y=result[:,1], px=result[:,2], py=result[:,3],color=color[i],title=title,axs=axs)
+        plt.show()
     
     if args.leapfrog:
         print("Integrate equations of motion using Leap-Frog")
-        result = henon_heiles(leap_frog,var,initial_conditions, t_values, dt)
+        fig, axs = plt.subplots(1, 3, figsize=(15, 6), sharex=False, sharey=False)
+        for i in range(0,int(len(initial_conditions)),1):
+            result = henon_heiles(leap_frog,var,initial_conditions[i], t_values, dt)
+            pm.plot_henon_heiles(t_values,x=result[:,0], y=result[:,1], px=result[:,2], py=result[:,3],color=color[i],title=title,axs=axs)
+        plt.show()
     
     if args.euler:
         print("Integrate equations of motion using Euler")
-        result = henon_heiles(euler,var,initial_conditions, t_values,dt)  
-        print(result)
-    
-    pm.plot_henon_heiles(t_values,x=result[:,0], y=result[:,1], px=result[:,2], py=result[:,3],color=color,title=title)
-
+        fig, axs = plt.subplots(1, 3, figsize=(15, 6), sharex=False, sharey=False)
+        for i in range(0,int(len(initial_conditions)),1):
+            result = henon_heiles(euler,var,initial_conditions[i], t_values, dt)
+            pm.plot_henon_heiles(t_values,x=result[:,0], y=result[:,1], px=result[:,2], py=result[:,3],color=color[i],title=title,axs=axs)
+        plt.show()
 
 if __name__ == "__main__":
     main()
--- poincare_maps.py
+++ poincare_maps.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_henon_heiles(t, x, y, px, py, color, title):
+def plot_henon_heiles(t, x, y, px, py, color, title,axs):
     """
     Plot the trajectories and energy evolution of the Henon-Heiles system.
 
@@ -17,12 +17,12 @@ def plot_henon_heiles(t, x, y, px, py, color, title):
     Returns:
     None
     """
-    fig, axs = plt.subplots(1, 3, figsize=(15, 6), sharex=False, sharey=False)
+    ax1,ax2,ax3=axs
 
     # Plot trajectories in the x-y plane
-    axs[0].scatter(x, y, s=5, c=color)
-    axs[0].set_xlabel('x', fontsize=12)
-    axs[0].set_ylabel('y', fontsize=12)
+    ax1.scatter(x, y, s=1, c=color)
+    ax1.set_xlabel('x', fontsize=12)
+    ax1.set_ylabel('y', fontsize=12)
 
     # Create a Poincaré map by selecting points within a range of x values
     selected_y = []
@@ -32,16 +32,17 @@ def plot_henon_heiles(t, x, y, px, py, color, title):
             selected_y.append(y[i])
             selected_py.append(py[i])
 
-    axs[1].scatter(selected_y, selected_py, s=5, c=color)
-    axs[1].set_xlabel('y', fontsize=12)
-    axs[1].set_ylabel('py', fontsize=12)
-    axs[1].set_title(f'{title}', fontsize=16)
+    ax2.scatter(selected_y, selected_py, s=1, c=color,alpha=0.7)
+    ax2.set_xlabel('y', fontsize=12)
+    ax2.set_ylabel('py', fontsize=12)
+    ax2.set_title(f'{title}', fontsize=16)
 
     # Plot energy evolution over time
     hamiltonian = (px**2) / 2 + (py**2) / 2 + (x**2) / 2 + (y**2) / 2 + x**2 * y - (1/3) * y**3
     normalized_hamiltonian = hamiltonian / hamiltonian[0]
 
-    axs[2].plot(t, normalized_hamiltonian, color=color)
-    axs[2].set_xlabel('Time', fontsize=12)
-    axs[2].set_ylabel('Normalized Energy', fontsize=12)
-    plt.show()
+    ax3.plot(t, normalized_hamiltonian, color=color)
+    ax3.set_xlabel('Time', fontsize=12)
+    ax3.set_ylabel('Normalized Energy', fontsize=12)
+    
+    plt.tight_layout()
